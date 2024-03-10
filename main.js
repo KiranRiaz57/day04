9cbac5fb1f65a1edcbeb11b2657fad408d6d42da
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-10 22:08:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Q10 
+// kiran,2024-03-10
+// let my_Name: string = "kiran"
+// console.log("Hello",my_Name,"would you like to learn some typescript today?");
+// Q11
+// using array
+// let friendsName: string[]= ["Mahrang","Laiba","Saira"];
+// for(let i = 0; i < friendsName.length; i++){
+//   console.log(friendsName[i]);
+// }
+// Q12
+var friendsName = ["Mahrang", "Laiba", "Saira"];
+for (var i = 0; i < friendsName.length; i++) {
+    console.log("Hello", friendsName[i], "would you like to learn some typescript today?");
+}
---
+++ main.ts
@@ -0,0 +1,16 @@
+// Q10 
+// kiran,2024-03-10
+// let my_Name: string = "kiran"
+// console.log("Hello",my_Name,"would you like to learn some typescript today?");
+// Q11
+// using array
+// let friendsName: string[]= ["Mahrang","Laiba","Saira"];
+// for(let i = 0; i < friendsName.length; i++){
+    //   console.log(friendsName[i]);
+// }
+// Q12
+// let friendsName: string[]= ["Mahrang","Laiba","Saira"];
+// for(let i = 0; i < friendsName.length; i++){
+//  console.log("Hello",friendsName[i],"would you like to learn some typescript today?")
+
+// }
